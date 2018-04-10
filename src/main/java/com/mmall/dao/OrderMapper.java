79246e4fc6fa2d67db5c81a15ef7990f3c834bc1
Wuleijian <18650588246@163.com>
2018-04-11 02:11:27 +0800

    支付，订单管理模块开发

--- src/main/java/com/mmall/dao/OrderMapper.java
+++ src/main/java/com/mmall/dao/OrderMapper.java
@@ -1,6 +1,12 @@
 package com.mmall.dao;
 
+import com.github.pagehelper.PageInfo;
+import com.mmall.common.ServerResponse;
 import com.mmall.pojo.Order;
+import com.mmall.vo.OrderVo;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface OrderMapper {
     int deleteByPrimaryKey(Integer id);
@@ -14,4 +20,12 @@ public interface OrderMapper {
     int updateByPrimaryKeySelective(Order record);
 
     int updateByPrimaryKey(Order record);
+
+    Order selectByUserIdAndOrderNo(@Param("userId") Integer userId, @Param("orderNo") Long orderNo);
+
+    Order selectByOrderNo(Long orderNo);
+
+    List<Order> selectByUserId(Integer userId);
+
+    List<Order> selectAllOrder();
 }
